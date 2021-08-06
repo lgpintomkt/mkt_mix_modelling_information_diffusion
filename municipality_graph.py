31b9093bd9937718f5f03305b7943da9790a4ca4
LGPinto <lgpintomkt@gmail.com>
2021-08-06 02:18:26 +0100

    correct issues related with unknown municipalities

--- municipality_graph.py
+++ municipality_graph.py
@@ -1,17 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  3 22:32:59 2021
+
+@author: MMSETUBAL
+"""
+
 import networkx as nx
 from shapely.geometry import shape
 import fiona
 import itertools
 import pandas as pd
 
-#DATA SOURCE:
-#https://data.humdata.org/dataset/6ba099c6-350b-4711-9a65-d85a1c5e519c
-
 geoms = fiona.open("jpn_admbnda_adm2_2019.shp")
 G = nx.DiGraph()
 municipalities=dict()
 for municipality in geoms:
-    G.add_node(municipality['id'],name=municipality['properties']['ADM2_EN'])
+    if municipality['properties']['ADM2_PCODE'] == 'JP10009':
+        municipality['properties']['ADM2_EN'] = 'Minamimaki-mura (Gunma Pref.)'
+    mun_name=municipality['properties']['ADM1_EN'].strip('Â\xa0').replace("HyÅgo","Hyōgo").replace("Åita","Ōita")+", "+municipality['properties']['ADM2_EN']
+    G.add_node(municipality['id'],name=mun_name)
     municipalities[municipality['id']]=shape(municipality['geometry'])
 for id1,id2 in itertools.combinations(municipalities.keys(), 2):
     if id1 == id2:
@@ -20,7 +27,12 @@ for id1,id2 in itertools.combinations(municipalities.keys(), 2):
     poly2=municipalities[id2]
     if poly1.intersects(poly2):
         G.add_edge(id1,id2)
+        G.add_edge(id2,id1)
+#correction, Ishioka-machi in Fukushima and Tono-cho & Kadokawa-cho in Miyazaki doesn't seem to exist
+G.remove_node('407')
+G.remove_node('1803')
+G.remove_node('1804')
 A = nx.attr_matrix(G, node_attr="name")
 pd.DataFrame(A[0],index=A[1],columns=A[1]).to_excel('japan_municipalities_adj.xlsx')
-nx.draw(G)
+#nx.draw(G)
 nx.write_graphml(G,'japan_municipalities.xml')
